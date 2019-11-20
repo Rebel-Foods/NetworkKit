28a369b8d691e1242fec4744c28f9cda3fba60cf
Raghav Ahuja <raghavahuja@me.com>
2019-11-21 00:43:39 +0530

    Update Platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkKit",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v10),
+        .watchOS(.v4)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
